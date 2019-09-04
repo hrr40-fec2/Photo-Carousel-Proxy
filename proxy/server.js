2dede203abfa74dc3df7b9c1de5920889c50a701
Mr Myers <myersniiansah@gmail.com>
2019-09-04 18:45:41 -0400

    configured each repo

--- proxy/server.js
+++ proxy/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3003;
 
-app.use('./public');
+app.use(express.static('./public'));
 
 app.listen(port, () => {
   console.log(`now listening on port ${port}`);
